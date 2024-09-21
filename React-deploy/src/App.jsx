67004acb573cae92dd69ee4fba9e18eace6c7af7
khushbugreata <khushbumishra341@gmail.com>
2024-09-21 16:37:17 +0530

    done

---
+++ React-deploy/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+function App() {
+  
+  return (
+    <center>
+      hello khushbu
+     <p>tum kar logi</p>
+     <button>success</button>
+    </center>
+  )
+}
+
+export default App
